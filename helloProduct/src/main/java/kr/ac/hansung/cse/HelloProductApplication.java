8e182a880d515b0c84e30adbb2ebc6237759f36b
sayole <sylee8191@gmail.com>
2020-06-26 08:16:12 +0900

    initial commit

---
+++ helloProduct/src/main/java/kr/ac/hansung/cse/HelloProductApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloProductApplication.class, args);
+	}
+
+}
